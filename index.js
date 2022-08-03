d07d8303920ff3249c37802f9964d37f0746a9d4
+ <+>
2022-08-03 00:27:40 -0400

    commit tree 5a073eedcb09ab806bb0a7dd48f6ee73ca9a4ba0

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 function isPalindrome(word) {
+  const length = word.length
+  for( i=0; i<length; i++) {
+    console.log(word[i])
+    console.log(word[length -1])
+if (word[i] !== word[length - 1]){
+  return false
+}
+else{
+  return true
+}
+  }
   // Write your algorithm here
 }
 
+
 /* 
   Add your pseudocode here
+  if the paramter is a palindrome
+  get length of word
+  check that the indexes match
+  return true 
+  anything else 
+  return false
 */
 
 /*
   Add written explanation of your solution here
+1) find the lenght of the argument 
+2) iterate through the first half of the indexes of the word 
+3) if the characters do not match then return false 
+4) anything else returns true 
 */
 
 // You can run `node index.js` to view these console logs
@@ -17,7 +39,8 @@ if (require.main === module) {
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
-
+  console.log("Expecting:false")
+console.log("=>", isPalindrome("local"));
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
